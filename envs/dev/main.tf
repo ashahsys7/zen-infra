ab3cdd68cc288966f7ac9afce16ec02d0aea55bf
Amit <ashahsys7@gmail.com>
2026-06-10 14:04:02 +1000

    changed number of nodes

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -19,9 +19,9 @@ module "eks" {
   cluster_version    = "1.33"
   subnet_ids         = module.vpc.private_eks_subnet_ids
   node_instance_type = "t3.small"
-  desired_capacity   = 3
-  min_size           = 1
-  max_size           = 4
+  desired_capacity   = 5
+  min_size           = 3
+  max_size           = 8
 }
 
 module "rds" {
